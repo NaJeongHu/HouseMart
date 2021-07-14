38d9fa69660d813f5d794d0ba5aa2b7f582c9539
NaJeongHu <65152311+NaJeongHu@users.noreply.github.com>
2021-07-14 17:41:33 +0900

    sssss

--- app/src/main/java/com/example/publicdatacompetition/LoginActivity.java
+++ app/src/main/java/com/example/publicdatacompetition/LoginActivity.java
@@ -3,9 +3,11 @@ package com.example.publicdatacompetition;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,10 +16,13 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 
 import okhttp3.MediaType;
 import okhttp3.RequestBody;
 import retrofit2.Call;
+import retrofit2.Callback;
 import retrofit2.Response;
 
 public class LoginActivity extends AppCompatActivity {
@@ -39,55 +44,78 @@ public class LoginActivity extends AppCompatActivity {
                 mId = mEditId.getText().toString();
                 mPassword = mEditPassword.getText().toString();
 
-                validateUserTask mTask = new validateUserTask();
-                mTask.execute(mId,mPassword);
+//                validateUserTask mTask = new validateUserTask();
+//                mTask.execute(mId,mPassword);
 
+                RESTApi mRESTApi = RESTApi.retrofit.create(RESTApi.class);
+                mRESTApi.login(mId,mPassword).enqueue(new Callback<List<Map<String, Object>>>() {
+                    @Override
+                    public void onResponse(Call<List<Map<String, Object>>> call, Response<List<Map<String, Object>>> response) {
+                        if (response.isSuccessful()) {
+                            if (response.code() == 00) {
+                                Log.d("LoginActivity", "login successful !");
+                                Toast.makeText(getApplicationContext(),"로그인 성공", Toast.LENGTH_SHORT).show();
+                                Intent i = new Intent(LoginActivity.this, MainActivity.class);
+                                startActivity(i);
+                            } else {
+                                Log.d("LoginActivity", "login failure !" + response.code());
+                                Toast.makeText(getApplicationContext(),"로그인 실패 response code : " + response.code(), Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<List<Map<String, Object>>> call, Throwable throwable) {
+                        Log.d("LoginActivity", "retrofit failure !");
+                        Toast.makeText(getApplicationContext(),"통신 실패", Toast.LENGTH_SHORT).show();
+                    }
+                });
             }
         });
     }
 
-    private class validateUserTask extends AsyncTask<String, Void, String> {
-
-        @Override
-        protected String doInBackground(String... params) {
-            JSONObject json = new JSONObject();
-
-            try {
-                json.put("id",params[0]);
-                json.put("password",params[1]);
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
+//    private class validateUserTask extends AsyncTask<String, Void, String> {
+//
+//        @Override
+//        protected String doInBackground(String... params) {
+//            JSONObject json = new JSONObject();
+//
 //            try {
+//                json.put("id",params[0]);
 //                json.put("password",params[1]);
 //            } catch (JSONException e) {
 //                e.printStackTrace();
 //            }
-
-            RequestBody body = RequestBody.create(MediaType.parse("application/json; charset=utf-8"),json.toString());
-
-            try {
-                RESTApi mRESTApi = RESTApi.retrofit.create(RESTApi.class);
-                Call<auth> login = mRESTApi.login(body);
-                Response res = login.execute();
-
-                if(res.isSuccessful()){
-                    Intent i = new Intent(LoginActivity.this, MainActivity.class);
-                    startActivity(i);
-                } else{ }
-
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-            String res = null;
-            return res;
-        }//close doInBackground
-
-        @Override
-        protected void onPostExecute(String result) {
-
-        }//close onPostExecute
-
-    }// close validateUserTask
+////            try {
+////                json.put("password",params[1]);
+////            } catch (JSONException e) {
+////                e.printStackTrace();
+////            }
+//
+//            RequestBody body = RequestBody.create(MediaType.parse("application/json; charset=utf-8"),json.toString());
+//
+//            try {
+//                RESTApi mRESTApi = RESTApi.retrofit.create(RESTApi.class);
+//                Call<List<Map<String,Object>>> login = mRESTApi.login(body);
+//                Response res = login.execute();
+//
+//                if(res.isSuccessful()){
+//                    Intent i = new Intent(LoginActivity.this, MainActivity.class);
+//                    startActivity(i);
+//                } else{ }
+//
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//
+//            String res = null;
+//            return res;
+//        }//close doInBackground
+//
+//        @Override
+//        protected void onPostExecute(String result) {
+//
+//        }//close onPostExecute
+//
+//    }// close validateUserTask
 }
--- app/src/main/java/com/example/publicdatacompetition/RESTApi.java
+++ app/src/main/java/com/example/publicdatacompetition/RESTApi.java
@@ -1,21 +1,30 @@
 package com.example.publicdatacompetition;
 
+import java.util.List;
+import java.util.Map;
+
 import okhttp3.RequestBody;
 import retrofit2.Call;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.http.Body;
+import retrofit2.http.GET;
 import retrofit2.http.Headers;
 import retrofit2.http.POST;
+import retrofit2.http.Query;
 
 public interface RESTApi {
 
     @Headers(value = "Content-Type: application/json")
-    @POST(value = "login")
-    Call<auth> login(@Body RequestBody body);
+//    @POST("/loginRequest")
+//    Call<List<Map<String,Object>>> login(@Body RequestBody body);
+
+    @GET("/loginRequest")
+    Call<List<Map<String,Object>>> login(@Query("id") String id,
+                                           @Query("password") String password);
 
     public static final Retrofit retrofit = new Retrofit.Builder()
-            .baseUrl("http://52.79.236.212:5000/")
+            .baseUrl("http://122.37.239.49:9000/")
             .addConverterFactory(GsonConverterFactory.create())
             .build();
 }
