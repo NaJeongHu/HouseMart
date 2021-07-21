50923400adc95eae0f144f5c1bb72f8f27786ea4
seongjinah <5829341@naver.com>
2021-07-21 15:01:26 +0900

    1. add firebase register at JoinActivity 2. create ResetPasswordActivity.java

--- app/src/main/java/com/example/publicdatacompetition/JoinActivity.java
+++ app/src/main/java/com/example/publicdatacompetition/JoinActivity.java
@@ -1,19 +1,39 @@
 package com.example.publicdatacompetition;
 
+import android.app.ProgressDialog;
+import android.content.ContentResolver;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
+import android.webkit.MimeTypeMap;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.StorageTask;
+import com.google.firebase.storage.UploadTask;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -25,6 +45,9 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.HashMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import okhttp3.MediaType;
 import okhttp3.MultipartBody;
@@ -36,19 +59,27 @@ import retrofit2.Response;
 
 public class JoinActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private EditText mEditId, mEditPassword, mEditPasswordConfirm, mEditPhoneNumber, mEditName, mEditNicname;
+    private EditText mEditEmail, mEditPassword, mEditPasswordConfirm, mEditPhoneNumber, mEditName, mEditNicname;
     private ImageView mIvPicture;
     private Button mJoinButton;
-    private static final int REQUEST_CODE = 0;
+    private static final int IMAGE_REQUEST = 0;
     private Bitmap img;
     private String mId, mPassword, mPasswordConfirm, mPhoneNumber, mName, mNickname;
+
+    private FirebaseAuth auth;
+    private DatabaseReference reference;
+    private FirebaseUser fuser;
+
+    private StorageReference storageReference;
+    private Uri imageUri;
+    private StorageTask uploadTask;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_join);
 
-        mEditId = findViewById(R.id.edit_join_id);
+        mEditEmail = findViewById(R.id.edit_join_email);
         mEditPassword = findViewById(R.id.edit_join_password);
         mEditPasswordConfirm = findViewById(R.id.edit_join_password_confirm);
         mEditPhoneNumber = findViewById(R.id.edit_join_phonenumber);
@@ -56,6 +87,9 @@ public class JoinActivity extends AppCompatActivity implements View.OnClickListe
         mEditNicname = findViewById(R.id.edit_join_nickname);
         mJoinButton = findViewById(R.id.btn_join_join);
         mIvPicture = findViewById(R.id.iv_join_picture);
+
+        auth = FirebaseAuth.getInstance();
+        storageReference = FirebaseStorage.getInstance().getReference("uploads");
 
         mIvPicture.setOnClickListener(this);
         mJoinButton.setOnClickListener(this);
@@ -68,18 +102,153 @@ public class JoinActivity extends AppCompatActivity implements View.OnClickListe
                 Intent intent = new Intent();
                 intent.setType("image/*");
                 intent.setAction(Intent.ACTION_GET_CONTENT);
-                startActivityForResult(intent, REQUEST_CODE);
+                startActivityForResult(intent, IMAGE_REQUEST);
                 break;
+
             case R.id.btn_join_join:
-                // 에딧텍스트 다 채워져 있는지, 무결성 검사 진행해야함.
-                // 비트맵 null인지 아닌지 검사해서 있으면 서버로 보내고, 없으면 null 서버로 보냄
-                if (img != null) {
-                    sendUserInfoToServer();
-                } else {
-                    Toast.makeText(getApplicationContext(),"사진을 넣어주세요", Toast.LENGTH_SHORT).show();
+                String txt_email = mEditEmail.getText().toString();
+                String txt_password = mEditPassword.getText().toString();
+                String txt_password_confirm = mEditPasswordConfirm.getText().toString();
+                String txt_username = mEditName.getText().toString();
+                String txt_nickname = mEditNicname.getText().toString();
+                String txt_phone = mEditPhoneNumber.getText().toString();
+
+                if(TextUtils.isEmpty(txt_username) || TextUtils.isEmpty(txt_email) || TextUtils.isEmpty(txt_password) || TextUtils.isEmpty(txt_nickname) || TextUtils.isEmpty(txt_phone)){
+                    Toast.makeText(this, "모든 정보를 채워주세요", Toast.LENGTH_SHORT).show();
+                } else if(!checkPass(txt_password)){
+                    Toast.makeText(this, "비밀번호는 영문, 숫자를 포함하여 8~20자로 작성해주세요.", Toast.LENGTH_SHORT).show();
+                } else if(!txt_password.equals(txt_password_confirm)){
+                    Toast.makeText(this, "비밀번호 재입력이 틀렸습니다.", Toast.LENGTH_SHORT).show();
+                }else {
+                    register(txt_email, txt_password, txt_username, txt_nickname, txt_phone);
                 }
                 break;
         }
+    }
+
+    private File savebitmap(Bitmap bmp) {
+        String extStorageDirectory = Environment.getExternalStorageDirectory().toString();
+        OutputStream outStream = null;
+        // String temp = null;
+        File file = new File(extStorageDirectory, "temp.png");
+        if (file.exists()) {
+            file.delete();
+            file = new File(extStorageDirectory, "temp.png");
+        }
+        try {
+            outStream = new FileOutputStream(file);
+            bmp.compress(Bitmap.CompressFormat.PNG, 100, outStream);
+            outStream.flush();
+            outStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+        return file;
+    }
+
+    private String getFileExtension(Uri uri) {
+        ContentResolver contentResolver = JoinActivity.this.getContentResolver();
+        MimeTypeMap mimeTypeMap = MimeTypeMap.getSingleton();
+        return mimeTypeMap.getExtensionFromMimeType(contentResolver.getType(uri));
+    }
+
+    private void uploadImage() {
+        final ProgressDialog pd = new ProgressDialog(JoinActivity.this); //context에 표시하는 progressdialog 인스턴스 생성
+        pd.setMessage("Uploading"); //progressdialog에 나타낼 text
+        pd.show(); //progressdialog를 보여주기
+
+        if(imageUri != null) { //intent의 결과로 imageUri가 넘어왔다면,
+            final StorageReference fileReference = storageReference.child(System.currentTimeMillis() //FirebaseStorage 내부에 upload 내부에
+                    +"."+getFileExtension(imageUri)); //"현재시간.형식"라는 이름의 StorageReference 만들기
+
+            uploadTask = fileReference.putFile(imageUri); //FirebaseStorage에 file을 추가하는 Task를 StorageTask에 할당
+            uploadTask.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
+                @Override
+                public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
+                    if(!task.isSuccessful()) {
+                        throw task.getException();
+                    }
+
+                    return fileReference.getDownloadUrl(); //file의 다운로드 url 반환
+                }
+            }).addOnCompleteListener(new OnCompleteListener<Uri>() {
+                @Override
+                public void onComplete(@NonNull Task<Uri> task) {
+                    if(task.isSuccessful()){
+                        Uri downloadUri = task.getResult();
+                        String mUri = downloadUri.toString();
+
+                        reference = FirebaseDatabase.getInstance().getReference("Users").child(fuser.getUid());
+                        HashMap<String, Object> map = new HashMap<>();
+                        map.put("imageURL", mUri);
+                        reference.updateChildren(map);
+
+                        pd.dismiss();
+                    } else {
+                        Toast.makeText(JoinActivity.this, "Failed!", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(JoinActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            });
+        } else {
+            Toast.makeText(JoinActivity.this, "No image selected", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void startLoginActivity() {
+        Intent intent = new Intent(JoinActivity.this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+        finish();
+    }
+
+    private void register(String email, String password, final String username, final String nickname, final String phone) {
+        auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            fuser = auth.getCurrentUser(); // 현재 사용자 가져오기
+                            fuser.sendEmailVerification()
+                                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+                                            if(task.isSuccessful()) {
+                                                uploadImage();
+
+                                                String userid = fuser.getUid(); // 현재 사용자의 userid
+
+                                                reference = FirebaseDatabase.getInstance().getReference("Users").child(userid);
+
+                                                HashMap<String, String> hashMap = new HashMap<>();
+                                                hashMap.put("id", userid);
+                                                hashMap.put("username", username);
+                                                hashMap.put("nickname", nickname);
+                                                hashMap.put("imageURL", "default");
+                                                hashMap.put("phone", phone);
+                                                hashMap.put("search", username.toLowerCase());
+
+                                                reference.setValue(hashMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                    @Override
+                                                    public void onComplete(@NonNull Task<Void> task) {
+                                                        if(task.isSuccessful()){
+                                                            sendUserInfoToServer();
+                                                        }
+                                                    }
+                                                });
+                                            }
+                                        }
+                                    });
+                        } else {
+                            Toast.makeText(JoinActivity.this, "다른 이메일을 입력해주세요", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 
     private void sendUserInfoToServer() {
@@ -121,7 +290,7 @@ public class JoinActivity extends AppCompatActivity implements View.OnClickListe
             e.printStackTrace();
         }
 
-        mId = mEditId.getText().toString();
+        mId = mEditEmail.getText().toString();
         mPassword = mEditPassword.getText().toString();
         mPasswordConfirm = mEditPasswordConfirm.getText().toString();
         mPhoneNumber = mEditPhoneNumber.getText().toString();
@@ -141,18 +310,16 @@ public class JoinActivity extends AppCompatActivity implements View.OnClickListe
                         Log.d("JoinActivity", "통신 성공 !" + response.headers());
 
                         String test = response.headers().get("code");
-                        if (test.equals("00")) {
-                            Intent i = new Intent(JoinActivity.this, MainActivity.class);
-                            startActivity(i);
-                        } else {
-                            Toast.makeText(getApplicationContext(),"회원가입 실패" + test , Toast.LENGTH_SHORT).show();
-                        }
 
-//                        Toast.makeText(getApplicationContext(),"통신 성공" + test , Toast.LENGTH_SHORT).show();
-//                        if (response.code() == 0) {
-//                            Intent i = new Intent(JoinActivity.this, MainActivity.class);
-//                            startActivity(i);
-//                        }
+                        if (test.equals("00")) {
+                            //로그인부터 해야할 것 같아서 LoginActivity로 넘어가도록 함
+                            Intent intent = new Intent(JoinActivity.this, MainActivity.class);
+                            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                            startActivity(intent);
+                            finish();
+                        } else {
+                            Toast.makeText(JoinActivity.this,"회원가입 실패" + test , Toast.LENGTH_SHORT).show();
+                        }
                     }
 
                     @Override
@@ -162,45 +329,41 @@ public class JoinActivity extends AppCompatActivity implements View.OnClickListe
                 });
     }
 
-    private File savebitmap(Bitmap bmp) {
-        String extStorageDirectory = Environment.getExternalStorageDirectory().toString();
-        OutputStream outStream = null;
-        // String temp = null;
-        File file = new File(extStorageDirectory, "temp.png");
-        if (file.exists()) {
-            file.delete();
-            file = new File(extStorageDirectory, "temp.png");
-        }
-        try {
-            outStream = new FileOutputStream(file);
-            bmp.compress(Bitmap.CompressFormat.PNG, 100, outStream);
-            outStream.flush();
-            outStream.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
-        }
-        return file;
-    }
-
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == REQUEST_CODE) {
-            if (resultCode == RESULT_OK) {
+
+        if(requestCode == IMAGE_REQUEST){
+            if(resultCode == RESULT_OK && data != null && data.getData() != null) {
+                imageUri = data.getData();
+
+                //change Uri to Bitmap
                 try {
-                    InputStream in = getContentResolver().openInputStream(data.getData());
+                    InputStream in = getContentResolver().openInputStream(imageUri);
 
                     img = BitmapFactory.decodeStream(in);
                     in.close();
-
-                    mIvPicture.setImageBitmap(img);
                 } catch (Exception e) {
-
+                    e.printStackTrace();
                 }
-            } else if (resultCode == RESULT_CANCELED) {
+
+                //set Image to mIvPicture
+                if (imageUri != null) {
+                    mIvPicture.setImageURI(imageUri);
+                }
+            } else if(resultCode == RESULT_CANCELED) {
                 Toast.makeText(this, "사진 선택 취소", Toast.LENGTH_LONG).show();
             }
         }
+    }
+
+    //password check policy
+    public boolean checkPass(String password){
+        String regex = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,20}$";
+        // 숫자, 대문자, 소문자 포함
+        // 8자 ~ 20자 허용
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(password);
+        return matcher.find();
     }
 }
--- app/src/main/java/com/example/publicdatacompetition/LoginActivity.java
+++ app/src/main/java/com/example/publicdatacompetition/LoginActivity.java
@@ -3,14 +3,18 @@ package com.example.publicdatacompetition;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -28,37 +32,70 @@ import retrofit2.Response;
 
 public class LoginActivity extends AppCompatActivity {
 
-    private EditText mEditId, mEditPassword;
+    private EditText mEditEmail, mEditPassword;
     private Button mLoginButton, mJoinButton;
-    private String mId, mPassword;
+    private TextView mResetPassword;
+
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_login);
-        mEditId = findViewById(R.id.edit_login_id);
+
+        mEditEmail = findViewById(R.id.edit_login_email);
         mEditPassword = findViewById(R.id.edit_login_password);
         mLoginButton = findViewById(R.id.btn_login);
         mJoinButton = findViewById(R.id.btn_join);
+        mResetPassword = findViewById(R.id.reset_password);
 
         mLoginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                mId = mEditId.getText().toString();
-                mPassword = mEditPassword.getText().toString();
+                //click할 때마다 새로 받아야하기 때문에 지역변수로 변경
+                String mEmail = mEditEmail.getText().toString();
+                String mPassword = mEditPassword.getText().toString();
 
-//                validateUserTask mTask = new validateUserTask();
-//                mTask.execute(mId,mPassword);
+                //아이디 혹은 비밀번호가 입력되지 않았다면 서버에 계정정보 확인하기 전에 처리
+                if (TextUtils.isEmpty(mEmail)) {
+                    Toast.makeText(LoginActivity.this, "이메일을 입력해주세요", Toast.LENGTH_SHORT).show();
+                } else if (TextUtils.isEmpty(mPassword)) {
+                    Toast.makeText(LoginActivity.this, "비밀번호를 입력해주세요", Toast.LENGTH_SHORT).show();
+                } else {
+                    //함수로 분리
+                    loginServer(mEmail, mPassword);
+                }
+            }
+        });
 
-                RESTApi mRESTApi = RESTApi.retrofit.create(RESTApi.class);
-                mRESTApi.login(mId,mPassword).enqueue(new Callback<ResponseBody>() {
-                    @Override
-                    public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
-                        String aa = response.headers().get("code");
+        mJoinButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, JoinActivity.class);
+                startActivity(intent);
+            }
+        });
 
-                        Toast.makeText(getApplicationContext(),"aa = "+response.headers() , Toast.LENGTH_SHORT).show();
-                        Log.d("LoginActivity", "통신 성공 !" + response.headers());
+        mResetPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, ResetPasswordActivity.class));
+            }
+        });
+    }
+
+    private void loginServer(String mEmail, String mPassword) {
+//      validateUserTask mTask = new validateUserTask();
+//      mTask.execute(mId,mPassword);
+
+        RESTApi mRESTApi = RESTApi.retrofit.create(RESTApi.class);
+        mRESTApi.login(mEmail,mPassword).enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                String aa = response.headers().get("code");
+
+                Toast.makeText(getApplicationContext(),"aa = "+response.headers() , Toast.LENGTH_SHORT).show();
+                Log.d("LoginActivity", "통신 성공 !" + response.headers());
 
 //                            if (response.code() == 0) {
 //                                Log.d("LoginActivity", "login successful !");
@@ -70,31 +107,21 @@ public class LoginActivity extends AppCompatActivity {
 //                                Toast.makeText(getApplicationContext(),"로그인 실패 response code : " + aa, Toast.LENGTH_SHORT).show();
 //                            }
 
-                        if (aa != null && aa.equals("00")) {
-                            Log.d("LoginActivity", "login successful !");
-                            Toast.makeText(getApplicationContext(),"로그인 성공", Toast.LENGTH_SHORT).show();
-                            Intent i = new Intent(LoginActivity.this, MainActivity.class);
-                            startActivity(i);
-                        } else {
-                            Log.d("LoginActivity", "login failure !" + response.code());
-                            Toast.makeText(getApplicationContext(),"로그인 실패 response code : " + aa, Toast.LENGTH_SHORT).show();
-                        }
-                    }
-
-                    @Override
-                    public void onFailure(Call<ResponseBody> call, Throwable throwable) {
-                        Log.d("LoginActivity", "retrofit failure !"+throwable);
-                        Toast.makeText(getApplicationContext(),"통신 실패", Toast.LENGTH_SHORT).show();
-                    }
-                });
+                if (aa != null && aa.equals("00")) {
+                    Log.d("LoginActivity", "login successful !");
+                    Toast.makeText(getApplicationContext(),"로그인 성공", Toast.LENGTH_SHORT).show();
+                    Intent i = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(i);
+                } else {
+                    Log.d("LoginActivity", "login failure !" + response.code());
+                    Toast.makeText(getApplicationContext(),"로그인 실패 response code : " + aa, Toast.LENGTH_SHORT).show();
+                }
             }
-        });
 
-        mJoinButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(LoginActivity.this, JoinActivity.class);
-                startActivity(intent);
+            public void onFailure(Call<ResponseBody> call, Throwable throwable) {
+                Log.d("LoginActivity", "retrofit failure !"+throwable);
+                Toast.makeText(getApplicationContext(),"통신 실패", Toast.LENGTH_SHORT).show();
             }
         });
     }
