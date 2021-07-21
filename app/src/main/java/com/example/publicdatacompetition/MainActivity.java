6f15f2b3f150fdcacf2acc45211a4c94bc1a495a
NaJeongHu <65152311+NaJeongHu@users.noreply.github.com>
2021-07-21 16:28:25 +0900

    add gps tracker

--- app/src/main/java/com/example/publicdatacompetition/MainActivity.java
+++ app/src/main/java/com/example/publicdatacompetition/MainActivity.java
@@ -1,24 +1,41 @@
 package com.example.publicdatacompetition;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private Button btn_info;
+    private GPSTracker gps;
+
+    public double mLatitude;
+    public double mLongitude;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // todo data binding 없이 작동하는지 확인
-        btn_info = findViewById(R.id.btn_main_info);
-        btn_info.setOnClickListener(this);
+        getpermisson();
+        getLocation();
+
+        // todo : data binding 없이 작동하는지 확인
     }
 
     @Override
@@ -36,6 +53,36 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 Intent iii = new Intent(MainActivity.this, UploadActivity.class);
                 startActivity(iii);
                 break;
+        }
+    }
+
+    private void getLocation() {
+        gps = new GPSTracker(MainActivity.this, this);
+        mLatitude = 0;
+        mLongitude = 0;
+
+        if (gps.canGetLocation()) {
+            mLatitude = gps.getLatitude();
+            mLongitude = gps.getLongitude();
+
+            if(mLatitude == 0 || mLongitude == 0){
+                Toast.makeText(getApplicationContext(), "GPS 활용 거부로 인해 초기위치값이 경북대로 설정되었습니다", Toast.LENGTH_LONG).show();
+                mLatitude = 35.887515;
+                mLongitude = 128.611553;
+            }
+        } else {
+            gps.showSettingsAlert();
+        }
+    }
+
+    private void getpermisson() {
+        int permiCheck_loca = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
+
+        if (permiCheck_loca == PackageManager.PERMISSION_DENIED) {
+            Log.d("위치 권한 없는 상태", "");
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 0);
+        } else {
+            Log.d("위치 권한 있는 상태", "");
         }
     }
 }
--- app/src/main/java/com/example/publicdatacompetition/MapActivity.java
+++ app/src/main/java/com/example/publicdatacompetition/MapActivity.java
@@ -12,5 +12,4 @@ public class MapActivity extends AppCompatActivity {
         setContentView(R.layout.activity_map);
 
     }
-
 }
