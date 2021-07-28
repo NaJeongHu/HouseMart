18bd5c35bcefd11f732f1c914f467bb1cf47098f
SeJin <tpwls337@naver.com>
2021-07-29 00:50:52 +0900

    가계약서 레이아웃

---
+++ app/src/main/java/com/example/publicdatacompetition/MakeContractActivity.java
@@ -0,0 +1,14 @@
+package com.example.publicdatacompetition;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MakeContractActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contract_upload);
+    }
+}
